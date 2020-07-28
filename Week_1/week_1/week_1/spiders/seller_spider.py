91be86463061e08c8391aa214db1df3ec0ee03cb
hrithik18236 <1hrithikmalhotra.gmail.com>
2020-07-28 23:52:28 +0530

    Changing site to Seller Central

--- Week_1/week_1/week_1/spiders/seller_spider.py
+++ Week_1/week_1/week_1/spiders/seller_spider.py
@@ -1,16 +1,14 @@
 import scrapy
 
-
-class CarSpider(scrapy.Spider):
-    name = "quotes"
+class SellerCentral(scrapy.Spider):
+    name = "SellerCentral"
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'https://community.cartalk.com/'
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url = url, callback = self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
